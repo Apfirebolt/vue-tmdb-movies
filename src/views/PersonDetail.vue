1db581c922881c8d95461eb23bd1cf92b52adc27
Apfirebolt <aspper20@gmail.com>
2025-04-13 15:19:28 +0530

    more details added to person

--- src/views/PersonDetail.vue
+++ src/views/PersonDetail.vue
@@ -68,6 +68,57 @@
     <div v-else class="text-center mt-4">
       <p>No images available for this person.</p>
     </div>
+
+    <div v-if="taggedImages.results && taggedImages.results.length" class="row mt-4">
+      <h3 class="text-center mb-3">Tagged Images</h3>
+      <div
+        v-for="(image, index) in taggedImages.results"
+        :key="index"
+        class="col-md-4 mb-3"
+      >
+        <img
+          :src="'https://image.tmdb.org/t/p/w500' + image.file_path"
+          class="img-fluid rounded"
+          :alt="'Tagged Image ' + (index + 1)"
+        />
+      </div>
+    </div>
+
+    <div v-if="credits.cast && credits.cast.length" class="row mt-4">
+      <h3 class="text-center mb-3">Movie Credits</h3>
+      <div
+        v-for="(movie, index) in credits.cast"
+        :key="index"
+        class="col-md-4 mb-3"
+      >
+        <div class="card text-secondary">
+          <img
+            v-if="movie.poster_path"
+            :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
+            class="card-img-top"
+            :alt="movie.title"
+          />
+          <div v-else class="card-img-top bg-secondary text-white d-flex justify-content-center align-items-center" style="height: 300px;">
+            Poster not available
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{{ movie.name }}</h5>
+            <p class="card-text">
+              <strong>Media Type:</strong> {{ movie.media_type || "N/A" }}
+            </p>
+            <p class="card-text">
+              <strong>Character:</strong> {{ movie.character || "N/A" }}
+            </p>
+            <p class="card-text">
+              <strong>Release Date:</strong> {{ movie.release_date || "N/A" }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="text-center mt-4">
+      <p>No movie credits available for this person.</p>
+    </div>
   </div>
 </template>
 
@@ -81,6 +132,8 @@ import Loader from "../components/Loader.vue";
 const personStore = usePersonStore();
 const route = useRoute();
 const personImages = ref({});
+const taggedImages = ref({});
+const credits = ref({});
 const personId = route.params.id; // Assuming the route parameter is named 'id'
 
 const isLoading = computed(() => personStore.isLoading);
@@ -101,10 +154,42 @@ const getPersonImages = (personId) => {
     });
 };
 
+const getTaggedImages = (personId) => {
+  httpClient
+    .get(`/person/${personId}/tagged_images`, {
+      params: {
+        api_key: import.meta.env.VITE_APP_KEY,
+      },
+    })
+    .then((response) => {
+      taggedImages.value = response.data;
+    })
+    .catch((error) => {
+      console.error("Error fetching tagged images:", error);
+    });
+};
+
+const getPersonCredits = (personId) => {
+  httpClient
+    .get(`/person/${personId}/combined_credits`, {
+      params: {
+        api_key: import.meta.env.VITE_APP_KEY,
+      },
+    })
+    .then((response) => {
+      credits.value = response.data;
+    })
+    .catch((error) => {
+      console.error("Error fetching person credits:", error);
+    });
+};
+
 onMounted(() => {
   if (personId) {
     personStore.getPersonDetails(personId);
     getPersonImages(personId);
+    getTaggedImages(personId);
+    getPersonCredits(personId);
   }
 });
 </script>
