0df4ea418cbaf67e4a2f47f74b5eebc6a7773e94
Apfirebolt <aspper20@gmail.com>
2025-04-14 12:17:14 +0530

    added vercel deployment

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from "./App.vue";
 import { createPinia } from 'pinia'
 import router from './routes'
 // import scss/_custom.scss';
-import "./assets/scss/_custom.scss";
-import "./assets/bootstrap.min.css";
+// import "./assets/scss/_custom.scss";
+// import "./assets/bootstrap.min.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
